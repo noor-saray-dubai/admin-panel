b3c878c9c622f81a9e0a133fbfe71fa7c35e481f
noor-saray-dubai <tech@noorsaray.com>
2025-08-11 16:10:32 +0400

    hyueo

--- app/dashboard/blogs/page.tsx
+++ app/dashboard/blogs/page.tsx
@@ -1,10 +1,11 @@
 import { BlogsPage } from "../../../components/blogs-page"
 import AuthWrapper from "../../../components/auth-wrapper"
+import { Suspense } from "react"
 
 export default function Blogs() {
   return (
-   
+     <Suspense fallback={<div>Loading...</div>}>
       <BlogsPage />
-    
+    </Suspense>
   )
 }
