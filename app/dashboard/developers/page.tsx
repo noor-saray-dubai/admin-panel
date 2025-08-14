4ce8fb41da988d856f226d6e9c8f29e935b16847
noor-saray-dubai <tech@noorsaray.com>
2025-08-14 11:19:07 +0400

    gyuirl

--- app/dashboard/developers/page.tsx
+++ app/dashboard/developers/page.tsx
@@ -1,10 +1,11 @@
 import { DevelopersPage } from "@/components/developers-page";
+import { Suspense } from "react";
 
 
 export default function Developers() {
   return (
-    
+    <Suspense fallback={<div>Loading...</div>}>
       <DevelopersPage />
-   
+   </Suspense>
   )
 }
--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -1,11 +1,13 @@
 import { ProjectsPage } from "@/components/projects-page";
+import { Suspense } from "react";
 
 
 
 export default function Projects() {
   return (
-   
+   <Suspense fallback={<div>Loading...</div>}> 
       <ProjectsPage />
+    </Suspense>
     
   )
 }
